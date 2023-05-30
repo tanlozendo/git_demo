01be759d63608964818a5fa7d5369d2abb63ee33
leonard <tan.lozendo@gmail.com>
2023-05-30 17:53:09 +0800

    Architect Asian Development

--- lessons/lesson7.js
+++ lessons/lesson7.js
@@ -7,7 +7,18 @@
 for(let i = 0; i<5; i++){
     console.log("Hello world " + i)
 }
+for(let i = 0; i<5; i++){
+    console.log("Hello world " + i)
+}
 
+console.log("heres the new code need to commit")
+console.log("heres the new code need to commit")
 console.log("heres the new code need to commit")
 
 // this another comment
+
+const greet =()=>{
+    return "hello world"
+}
+
+console.log(greet)
